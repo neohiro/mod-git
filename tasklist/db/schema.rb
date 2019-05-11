f1c9c6e860a2acac44932a2436a7c658c6a08c0d
neoo <foundation.system.Z@gmail.com>
2019-05-11 13:36:28 +0000

    mod4

--- tasklist/db/schema.rb
+++ tasklist/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_063313) do
+ActiveRecord::Schema.define(version: 2019_05_10_161001) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
